1e34523f820e2e3ca01f311bc561baab876beab6
Pawel Mstowski <pawel.mstowski@gmail.com>
2025-05-20 08:53:55 +0200

    Update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.o.background = "dark"
+
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.colorcolumn = "120"
+opt.scrolloff = 20
+opt.spelllang = { "en", "pl" }
+opt.wrap = true
